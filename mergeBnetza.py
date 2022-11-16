9b524b7d70ca06ede52b847828cbb00b2f59c877
Andreas Kugel <andreas.kugel@web.de>
2022-11-16 16:51:56 +0100

    more analysis

--- mergeBnetza.py
+++ mergeBnetza.py
@@ -4,8 +4,8 @@ import pandasgui as gui
 import matplotlib.pyplot as plt
 
 dwd = pd.read_csv("dwd-mean.csv")
-ind = pd.read_json("../bnetza/gasind.json")
-prv = pd.read_json("../bnetza/gashouse.json")
+ind = pd.read_json("gasind.json")
+prv = pd.read_json("gashouse.json")
 
 bi = pd.DataFrame(columns=["yr","kw","ind"])
 for i in ind.itertuples():
@@ -44,12 +44,45 @@ if len(dwd) > len(bna):
     dwd.drop(index=list(range(len(bna),len(dwd))),inplace=True)
     
 klima = pd.merge(bna,dwd)
+# ratio of gas/temp °K
+klima["ir"] = klima.ind/(klima.Mean + 273)
+klima["pr"] = klima.prv/(klima.Mean + 273)
+
+# special date string
+def weekDt(x):
+    return f'{int(x["kw"])}/{int(x["yr"])}'
+
+klima["wk"] = klima.apply(weekDt,axis=1)
+
+k = klima[["ir","pr","kw","yr"]]
+#kk = k.sort_values(by=["kw","yr"]).groupby(by="yr").diff().fillna(0)
 klima["t"] = klima.Mean * 100 # scale temperature by 100
 klima.to_csv("klima.csv")
-fig, axes = plt.subplots(nrows=2, ncols=1,sharex=True)
+fig, axes = plt.subplots(nrows=3, ncols=1,sharex=True)
 
 klima.plot(ax=axes[0],y=["ind","prv"])
 klima.plot(ax=axes[1],y=["Mean","Min","Max"])
+klima.plot(ax=axes[2],y=["ir","pr"])
 
 plt.show()
 
+# more analysis
+k = klima.groupby(by="yr")[["ir","pr","kw","ind","prv","t","Mean"]]
+
+yrs = [x for x,_ in k]
+print("Years: ",yrs)
+
+k21 = k.get_group(2021)
+k22 = k.get_group(2022)
+
+k21.set_index("kw",inplace=True)
+k22.set_index("kw",inplace=True)
+
+kk = k21.join(k22,how="inner",rsuffix="_r")
+
+kk["idelta"] = (kk.ir_r - kk.ir)/kk.ir * 100.0
+kk["pdelta"] = (kk.pr_r - kk.pr)/kk.pr * 100.0
+
+kk.plot(y=["idelta","pdelta","Mean"])
+
+plt.show()
